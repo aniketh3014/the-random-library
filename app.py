2eeb8f16ad93a986485f9395c485ba759557eac1
Aniket <ghoshaniket1000@gmail.com>
2023-08-03 20:01:02 +0530

    added index file and more python code

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    string = random_string(1000)
+    print(string)
+    return render_template("index.html", placeholder=string)
